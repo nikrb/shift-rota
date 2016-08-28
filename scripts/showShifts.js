d36a4b961833354989935f0330d331fe1e1e4e60
nik <nik_rb@yahoo.com>
2016-08-28 16:48:09 +0100

    check date between query

--- scripts/showShifts.js
+++ scripts/showShifts.js
@@ -1,4 +1,8 @@
 #!/usr/bin/mongo
 
 var db = new Mongo().getDB("rotadb");
-db.shift.find().forEach( printjson);
+// db.shift.find().forEach( printjson);
+db.shift.find( { start_time: {
+  $gt : ISODate( "2016-08-30"),
+  $lt: ISODate( "2016-09-03")
+}}).forEach( printjson);
