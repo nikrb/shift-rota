e5e49de0e4c79e7c0048f5358ec5ee7867780c27
nik <nik_rb@yahoo.com>
2016-09-01 23:57:27 +0100

    add some fields and show/hide to the shift dialogue

--- src/js/components/shift/ShiftDialogue.js
+++ src/js/components/shift/ShiftDialogue.js
@@ -1,13 +1,7 @@
 import React from 'react';
+import moment from 'moment';
 
 export default class ShiftDialogue extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-
-    };
-  }
-
   render(){
     const overlayStyles = {
       position: 'fixed',
@@ -33,7 +27,8 @@ export default class ShiftDialogue extends React.Component {
       boxShadow: '0px 0px 4px rgba(0,0,0,.14),0px 4px 8px rgba(0,0,0,.28)',
     };
     const dialogueTitle = {
-      marginTop: "2em"
+      marginTop: "2em",
+      fontSize: "1.5em"
     }
     const closeButtonStyle = {
       cursor: 'pointer',
@@ -42,6 +37,12 @@ export default class ShiftDialogue extends React.Component {
       right: '10px',
       top: '0px',
     };
+    let start_time_string = "";
+    let end_time_string = "";
+    if( this.props.selected_shift){
+      start_time_string = moment( this.props.selected_shift.start_time).format( "DD-MMM-YYYY HH:mm");
+      end_time_string = moment( this.props.selected_shift.end_time).format( "DD-MMM-YYYY HH:mm");
+    }
     return (this.props.selected_shift !== null )? (
       <div style={overlayStyles} onClick={this.props.onClosed}>
         <div style={dialogStyles}  className="modal-dialogue-close" >
@@ -56,7 +57,15 @@ export default class ShiftDialogue extends React.Component {
           <form className="well">
             <div className="form-group">
               <label htmlFor="client_initials" className="col-sm-2 control-label">Client</label>
-              <input id="client_initials" value={this.props.selected_shift.initials} />
+              <input id="client_initials" value={this.props.selected_shift.client.initials} readOnly />
+            </div>
+            <div className="form-group">
+              <label htmlFor="start_time" className="col-sm-2 control-label" >Start</label>
+              <input id="start_time" value={start_time_string} readOnly />
+            </div>
+            <div className="form-group">
+              <label htmlFor="end_time" className="col-sm-2 control-label" >End</label>
+              <input id="end_time" value={end_time_string} readOnly />
             </div>
             <div className="form-group" >
               <label htmlFor="delete_button" className="col-sm-2" />
