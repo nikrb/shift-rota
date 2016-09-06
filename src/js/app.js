832f4c6ad7955cefb7cc5474ed7eea707021680d
nik <nik_rb@yahoo.com>
2016-09-06 11:58:08 +0100

    use react router browserHistory instead of hashHistory

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // TODO: we don't use style/style2 but seems to import all the css
 import style from '../scss/app.scss';
@@ -16,7 +16,7 @@ import Settings from './pages/Settings';
 
 const app = document.getElementById('app');
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Layout} >
       <IndexRoute component={Rota} ></IndexRoute>
       <Route path="shifts" name="shifts" component={Shifts} ></Route>
