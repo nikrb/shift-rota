0483ecbc4ce59f69008c6f4793310cab040a3ba7
nik <nik_rb@yahoo.com>
2016-10-08 12:17:50 +0100

    facilitate variable number of weeks in month. closes #11

--- src/js/components/shift/ShiftTable.js
+++ src/js/components/shift/ShiftTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 import ShiftTableSection from './ShiftTableSection';
 
@@ -10,21 +11,21 @@ export default class ShiftTable extends React.Component {
     first_date.setDate( 1);
     const first_day = first_date.getDay();
     const diff = first_date.getDate() - first_day + ( first_day == 0 ? -6:1);
+    const start_week = moment( first_date).week();
+    const end_week = moment( first_date).endOf( 'month').week();
+    const week_count = end_week - start_week;
+    console.log( "week count:", week_count);
 
-    const week_commencing = [
-      new Date( first_date.setDate( diff)),
-      new Date( first_date.setDate( first_date.getDate()+7)),
-      new Date( first_date.setDate( first_date.getDate()+7)),
-      new Date( first_date.setDate( first_date.getDate()+7)),
-      new Date( first_date.setDate( first_date.getDate()+7))
-    ];
+    let week_commencing = [];
+    week_commencing.push( new Date( first_date.setDate(diff)));
+    for( let i = 0; i<week_count; i++){
+      week_commencing.push( new Date( first_date.setDate( first_date.getDate()+7)));
+    }
     const rows = week_commencing.map( ( start_date, ndx) => {
-      const end_date = new Date( start_date).setDate( start_date.getDate()+8);
-      const start_day = start_date.getDate();
       let day_cols = [];
       let night_cols = [];
       if( this.props.shifts.length){
-        for( let i =0; i < 7; i++){
+        for( let i =0; i < 7 && (ndx*7+i) < this.props.shifts.length; i++){
           const shift = this.props.shifts[ndx*7 + i];
           day_cols.push( shift.day);
           night_cols.push( shift.night);
@@ -35,10 +36,9 @@ export default class ShiftTable extends React.Component {
           night_cols.push( null);
         }
       }
-      const key_ndx = ndx; // new Date().getTime();
       return (
         <div className="well">
-          <ShiftTableSection start_date={start_date}
+          <ShiftTableSection key={ndx} start_date={start_date}
                           day_cols={day_cols}
                           night_cols={night_cols}
                           onShiftClicked={this.props.shiftClicked} />
